6f6329a9f4fba93475b47bc42a8973f90f1453e1
rafaelmacielrsm <rafaelmacielrsm@gmail.com>
2017-07-04 17:21:33 -0300

    Adds user first specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryGirl.build(:user) }
+
+  it { expect(subject).to respond_to(:email) }
+  it { expect(subject).to respond_to(:password) }
+  it { expect(subject).to respond_to(:password_confirmation) }
+
+  it { expect(subject).to be_valid }
 end
