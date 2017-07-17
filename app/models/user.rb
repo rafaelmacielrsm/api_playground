2e05321a7e4cef8d1f75175ec5791b214ab16044
rafaelmacielrsm <rafaelmacielrsm@gmail.com>
2017-07-17 16:42:05 -0300

    Adds the auth_token to the user model
    - Creates a new migration to add a new column to the users table and its
      respective index.
    - Adds a uniqueness validation for auth_token.
    - Adds specs for theses changes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # callbacks
+  before_create :generate_authentication_token!
+
+  #model validations
+  validates :auth_token, uniqueness: true
+
+  # devises modules
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?( auth_token: auth_token )
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { FactoryGirl.build(:user) }
+  subject { FactoryGirl.build(:userx) }
 
   it { expect(subject).to respond_to(:email) }
   it { expect(subject).to respond_to(:password) }
   it { expect(subject).to respond_to(:password_confirmation) }
+  it { expect(subject).to respond_to(:auth_token) }
 
   it { expect(subject).to be_valid }
 
   context 'Validations' do
     #email validations
     it { expect(subject).to validate_presence_of(:email) }
+
     it { expect(subject).to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+
     it { expect(subject).to allow_value('example@domain.com').for(:email) }
+
     it { expect(subject).not_to allow_value('example@').for(:email) }
     #password validation
     it { expect(subject).to validate_confirmation_of(:password) }
+    #auth_token validation
+    it { expect( subject ).to validate_uniqueness_of(:auth_token) }
 
+    describe "#generate_authentication_token!" do
+      let(:existing_user) {
+        FactoryGirl.create(:user)
+          .update_attributes(auth_token: 'AUniqueToken123')}
+
+      let(:new_user) { FactoryGirl.build(:user, auth_token: 'AUniqueToken123') }
+
+      it 'should generate a unique token' do
+        allow(Devise).to receive(:friendly_token).and_return("AUniqueToken123")
+        subject.generate_authentication_token!
+        expect( subject.auth_token ).to eql("AUniqueToken123")
+      end
+    end
+    
   end
 end
