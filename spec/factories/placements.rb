dbea5c3f75338c8d5adf30fc0d60ee28b348f575
rafaelmacielrsm <rafaelmacielrsm@gmail.com>
2017-07-31 17:33:14 -0300

    Adds a method and a callback to decrement quantity
     - Adds a method to decrement quantity of a product when a placement is
       created, this method is called in a after_create callback.

--- spec/factories/placements.rb
+++ spec/factories/placements.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :placement do
     order nil
     product nil
+    quantity { Random.new.rand(1..3) }
   end
 end
--- spec/models/placement_spec.rb
+++ spec/models/placement_spec.rb
@@ -7,4 +7,17 @@ RSpec.describe Placement, type: :model do
 
   it { is_expected.to belong_to :order }
   it { is_expected.to belong_to :product }
+
+  describe '#decrement_product_quantity!' do
+    let(:user) { FactoryGirl.build :user }
+    let(:product) { FactoryGirl.build :product, quantity: 5 }
+    let(:order) { FactoryGirl.build :order, user: user }
+    let(:placement){
+      FactoryGirl.build :placement, order: order, product: product}
+
+    it 'should decrease the products quantity by the placement quantity' do
+      expect { placement.decrement_product_quantity! }.
+          to change{product.quantity}.by(- placement.quantity)
+    end
+  end
 end
